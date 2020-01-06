b8bf9eb1eff158a1c6f177dd6d9c71b4329ea86f
Dmitry Morozov <dvmorozov@hotmail.com>
2020-01-06 08:52:13 +0100

    License was changed from GPL to MPL. #9

--- package/NumericGrid.pas
+++ package/NumericGrid.pas
@@ -1,10 +1,10 @@
-{------------------------------------------------------------------------------
-    This software is distributed under GPL in the hope that it will
-    be useful, but WITHOUT ANY WARRANTY. Without even the warranty
-    of FITNESS FOR A PARTICULAR PURPOSE.
-
-    Copyright (C) D.Morozov (dvmorozov@hotmail.com)
-------------------------------------------------------------------------------}
+{------------------------------------------------------------------------------------------------------------------------
+    This software is distributed under MPL 2.0 https://www.mozilla.org/en-US/MPL/2.0/ in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR ANY PARTICULAR PURPOSE.
+    Copyright (C) Dmitry Morozov: dvmorozov@hotmail.com
+                        LinkedIn: https://www.linkedin.com/in/dmitry-morozov-79490a59/
+                        Facebook: https://www.facebook.com/dmitry.v.morozov
+------------------------------------------------------------------------------------------------------------------------}
 unit NumericGrid;
 
 interface
